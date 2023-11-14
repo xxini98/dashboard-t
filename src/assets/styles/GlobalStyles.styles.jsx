5ee3065bd16e4bb60924746eb67f489dc29f7c4f
zzining <iammes98@gmail.com>
2023-11-14 12:49:29 +0900

    수정2

--- src/assets/styles/GlobalStyles.styles.jsx
+++ src/assets/styles/GlobalStyles.styles.jsx
@@ -1,5 +1,5 @@
-import { createGlobalStyle } from 'styled-components';
-import reset from 'styled-reset';
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 const GlobalStyles = createGlobalStyle`
 
 ${reset}
@@ -45,12 +45,6 @@ ${reset}
         --header-height: 60px;
 
         --link-default: #F4F7FE;
-    }
-    body {
-         background: url(3.jpg) no-repeat 0 0 !important;
-         #root {
-             opacity: 0.9 !important;
-         }
     }
     *{
         box-sizing: border-box;
